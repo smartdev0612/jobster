1e9523f1c4fd74c42bcd5b6de34fe2f18f85b9bf
Smart Dev <danieldev0612@gmail.com>
2023-02-23 02:58:53 -0800

    setup react router

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -2,6 +2,7 @@ import React from 'react'
 import main from '../assets/images/main.svg'
 import Wrapper from '../assets/wrappers/LandingPage'
 import { Logo } from '../components'
+import { Link } from 'react-router-dom'
 
 const Landing = () => {
   return (
@@ -26,7 +27,9 @@ const Landing = () => {
             pour-over, dreamcatcher jean shorts truffaut letterpress biodiesel
             bushwick.
           </p>
-          <button className="btn btn-hero">Login/Register</button>
+          <Link to="/register" className="btn btn-hero">
+            Login/Register
+          </Link>
         </div>
         <img src={main} alt="job hunt" className="img main-img" />
       </div>
