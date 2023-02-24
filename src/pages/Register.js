4af5431ac2ad59d7f8e1deaeb90cb2ff5ed1c094
Smart Dev <danieldev0612@gmail.com>
2023-02-24 03:09:22 -0800

    install react-redux, setup user slice

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { Logo, FormRow } from '../components'
 import Wrapper from '../assets/wrappers/RegisterPage'
+import { toast } from 'react-toastify'
 
 const initialState = {
   name: '',
@@ -22,7 +23,7 @@ const Register = () => {
     e.preventDefault()
     const { name, email, password, isMember } = values
     if (!email || !password || (!isMember && !name)) {
-      console.log('Please fill out all fields')
+      toast.warning('Please fill out all fields')
     }
   }
 
