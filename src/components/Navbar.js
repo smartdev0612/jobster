5c6a779e50474e74c07bb7b95894c54921914760
Smart Dev <danieldev0612@gmail.com>
2023-02-26 19:51:46 -0800

    setup projected route

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,9 +3,10 @@ import Wrapper from '../assets/wrappers/Navbar'
 import { FaAlignLeft, FaUserCircle, FaCaretDown } from 'react-icons/fa'
 import Logo from './Logo'
 import { useDispatch, useSelector } from 'react-redux'
-import { toggleSidebar } from '../features/user/userSlice'
+import { toggleSidebar, logoutUser } from '../features/user/userSlice'
 
 const Navbar = () => {
+  const [showLogout, setShowLogout] = useState(false)
   const { user } = useSelector((store) => store.user)
   const dispatch = useDispatch()
 
@@ -27,17 +28,17 @@ const Navbar = () => {
           <button
             type="button"
             className="btn"
-            onClick={() => console.log('toggle logout dropdown')}
+            onClick={() => setShowLogout(!showLogout)}
           >
             <FaUserCircle />
             {user?.name}
             <FaCaretDown />
           </button>
-          <div className="dropdown show-dropdown">
+          <div className={showLogout ? 'dropdown show-dropdown' : 'dropdown'}>
             <button
               type="button"
               className="dropdown-btn"
-              onClick={() => console.log('logout user')}
+              onClick={() => dispatch(logoutUser())}
             >
               logout
             </button>
