3e5aadc7a1b2a2bc5863216ce69cdf793d12a333
Smart Dev <danieldev0612@gmail.com>
2023-03-04 11:46:52 -0800

    update all jobs page

--- src/features/allJobs/allJobsSlice.js
+++ src/features/allJobs/allJobsSlice.js
@@ -42,6 +42,14 @@ export const getAllJobs = createAsyncThunk(
 const allJobsSlice = createSlice({
   name: 'allJobs',
   initialState,
+  reducers: {
+    showLoading: (state) => {
+      state.isLoading = true
+    },
+    hideLoading: (state) => {
+      state.isLoading = false
+    },
+  },
   extraReducers: (builder) => {
     builder
       .addCase(getAllJobs.pending, (state) => {
@@ -57,5 +65,7 @@ const allJobsSlice = createSlice({
       })
   },
 })
+
+export const { showLoading, hideLoading } = allJobsSlice.actions
 
 export default allJobsSlice.reducer
