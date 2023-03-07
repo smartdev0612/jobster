3fc979ace38746b19684be43bbcdf8f2b9cc6d3c
Smart Dev <danieldev0612@gmail.com>
2023-03-06 18:47:56 -0800

    complete area and bar charts

--- src/components/BarChart.js
+++ src/components/BarChart.js
@@ -1,7 +1,26 @@
 import React from 'react'
+import {
+  ResponsiveContainer,
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+} from 'recharts'
 
-const BarChart = () => {
-  return <h2>BarChart</h2>
+const BarChartComponent = ({ data }) => {
+  return (
+    <ResponsiveContainer width="100%" height={300}>
+      <BarChart data={data} margin={{ top: 50 }}>
+        <CartesianGrid strokeDasharray="10 10" />
+        <XAxis dataKey="date" />
+        <YAxis allowDecimals={false} />
+        <Tooltip />
+        <Bar dataKey="count" fill="#3b82f6" barSize={75} />
+      </BarChart>
+    </ResponsiveContainer>
+  )
 }
 
-export default BarChart
+export default BarChartComponent
