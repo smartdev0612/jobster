0a3ebccd214f7814b5a11a8c4eeb6357d483cc13
Smart Dev <danieldev0612@gmail.com>
2023-03-07 19:17:18 -0800

    update param of allJobs request

--- src/features/allJobs/allJobsSlice.js
+++ src/features/allJobs/allJobsSlice.js
@@ -24,14 +24,14 @@ const initialState = {
 export const getAllJobs = createAsyncThunk(
   'allJobs/getJobs',
   async (_, thunkAPI) => {
-    let url = `/jobs`
+    const { page, search, searchStatus, searchType, sort } =
+      thunkAPI.getState().allJobs
+    let url = `/jobs?status=${searchStatus}&jobType=${searchType}&sort=${sort}&page=${page}`
+    if (search) {
+      url = url + `&search=${search}`
+    }
     try {
-      const resp = await customFetch.get(url, {
-        headers: {
-          Authorization: `Bearer ${thunkAPI.getState().user.user.token}`,
-        },
-      })
-      console.log(resp.data)
+      const resp = await customFetch.get(url)
       return resp.data
     } catch (error) {
       return thunkAPI.rejectWithValue('There was an error')
