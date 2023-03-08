784d551fb9317f5b8fe8d5fa21f0d2e5756a4dab
Smart Dev <danieldev0612@gmail.com>
2023-03-07 18:54:53 -0800

    setup page container, change page

--- src/features/allJobs/allJobsSlice.js
+++ src/features/allJobs/allJobsSlice.js
@@ -68,6 +68,9 @@ const allJobsSlice = createSlice({
     clearFilters: (state) => {
       return { ...state, ...initialFiltersState }
     },
+    changePage: (state, { payload }) => {
+      state.page = payload
+    },
   },
   extraReducers: (builder) => {
     builder
@@ -99,7 +102,12 @@ const allJobsSlice = createSlice({
   },
 })
 
-export const { showLoading, hideLoading, handleChange, clearFilters } =
-  allJobsSlice.actions
+export const {
+  showLoading,
+  hideLoading,
+  handleChange,
+  clearFilters,
+  changePage,
+} = allJobsSlice.actions
 
 export default allJobsSlice.reducer
